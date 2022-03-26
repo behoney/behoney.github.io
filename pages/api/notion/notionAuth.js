4fa7dd2261e39cdfaca0150b0eb9c3459d910245
behoney <bhoney1016@gmail.com>
2022-03-26 14:32:26 +0000

    [add] temporal dev API key

--- pages/api/notion/notionAuth.js
+++ pages/api/notion/notionAuth.js
@@ -1,3 +1,4 @@
+import { NOTION_KEY, NOTION_DATABASE_ID } from "../../APIkey/notionKey.js"
 const { Client } = require("@notionhq/client")
 
 // Initializing a client
@@ -9,33 +10,33 @@ const notion = new Client({
 (async () => {
     const databaseId = NOTION_DATABASE_ID;
     const response = await notion.databases.query({
-      database_id: databaseId,
-      filter: {
-        // or: [
-        //   {
-        //     property: 'In stock',
-        //     checkbox: {
-        //       equals: true,
-        //     },
-        //   },
-        //   {
-        //     property: 'Cost of next trip',
-        //     number: {
-        //       greater_than_or_equal_to: 2,
-        //     },
-        //   },
-        // ],
-      },
-      sorts: [
-        // {
-        //   property: 'Last ordered',
-        //   direction: 'ascending',
-        // },
-      ],
+        database_id: databaseId,
+        filter: {
+            // or: [
+            //   {
+            //     property: 'In stock',
+            //     checkbox: {
+            //       equals: true,
+            //     },
+            //   },
+            //   {
+            //     property: 'Cost of next trip',
+            //     number: {
+            //       greater_than_or_equal_to: 2,
+            //     },
+            //   },
+            // ],
+        },
+        sorts: [
+            // {
+            //   property: 'Last ordered',
+            //   direction: 'ascending',
+            // },
+        ],
     });
     console.log(response);
-  })();
-  
+})();
+
 
 
 
