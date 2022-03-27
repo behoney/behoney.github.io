94b14cb87106735c16ab1352d90a29aeb6274e61
behoney <bhoney1016@gmail.com>
2022-03-27 15:15:59 +0000

    [add] `getTitles` function is on testing

--- pages/api/notion/notionAuth.js
+++ pages/api/notion/notionAuth.js
@@ -29,9 +29,9 @@ async function addItem(text) {
 }
 
 export async function test() {
-    console.log("hello from notion Auth.js");
+    // console.log("hello from notion Auth.js");
 
-    const results = await (async () => {
+    const getTitles = async () => {
         const response = await notion.search({
             sort: {
                 direction: 'ascending',
@@ -45,55 +45,15 @@ export async function test() {
                 pages.push(result)
         }
 
-        for (const page of pages)
-        {
-
-            (async () => {
-                const pageId = page.id;
-                const response = await notion.pages.retrieve({ page_id: pageId });
-                console.log(response.properties.post);
-            })();
+        const titles = []
+        for (const page of pages) {
+            const pageId = page.id;
+            const response = await notion.pages.retrieve({ page_id: pageId });
+            titles.push(response.properties.post.title[0].plain_text);
         }
-    })();
-
-
-    // addItem("Yurts in Big Sur, California")
-
-    // Initializing a client
-
-    // (async () => {
-    //     const databaseId = NOTION_DATABASE_ID;
-    //     const response = await notion.databases.query({
-    //         database_id: databaseId,
-    //         filter: {
-    //             or: [
-    //                 {
-    //                     property: 'post',
-    //                     rich_text: {
-    //                         "contains": "Test"
-    //                     }
-    //                     // text: {
-    //                     //   equals: true,
-    //                     // },
-    //                 },
-    //                 // {
-    //                 //     property: 'Cost of next trip',
-    //                 //     number: {
-    //                 //         greater_than_or_equal_to: 2,
-    //                 //     },
-    //                 // },
-    //             ],
-    //         },
-    //         sorts: [
-    //             {
-    //                 property: 'Last ordered',
-    //                 direction: 'ascending',
-    //             },
-    //         ],
-    //     });
-    //     return response;
-
-    // })().then((res) => { return res });
-
-    return "huh?"
+        return titles;
+    };
+    const result = await getTitles()
+    console.log(result);
+    return result;
 }
